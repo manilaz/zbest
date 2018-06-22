b9d11c119c9ab78b053947efb8bb455eb6fa3d8a
manilaz <24566370@qq.com>
2018-06-23 00:07:35 +0800

    提交
    提交

--- jgroups/src/main/java/com/zbest/jgroups/listener/InitContextListener.java
+++ jgroups/src/main/java/com/zbest/jgroups/listener/InitContextListener.java
@@ -1,12 +1,11 @@
 package com.zbest.jgroups.listener;
 
-import com.alibaba.fastjson.JSON;
-import jdk.nashorn.api.scripting.JSObject;
 import org.jgroups.Address;
 import org.jgroups.JChannel;
 import org.jgroups.stack.IpAddress;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.json.JsonSimpleJsonParser;
 
 import javax.servlet.ServletContextEvent;
@@ -18,6 +17,9 @@ import javax.servlet.annotation.WebListener;
  */
 @WebListener
 public class InitContextListener implements ServletContextListener{
+
+    @Value("${jgroups.config.url}")
+    private String url;
 
     Logger logger = LoggerFactory.getLogger(InitContextListener.class);
 
@@ -35,11 +37,11 @@ public class InitContextListener implements ServletContextListener{
         try {
             Address address = new IpAddress("127.0.0.1:7800");
 
-            JChannel jChannel = new JChannel("/Users/zhangbin/tcp.xml");
+            JChannel jChannel = new JChannel(url);
 
             jChannel.connect("cs",address,1000*5);
 
-            logger.info("=============={}==================",JSON.toJSONString(jChannel.getView()));
+            logger.info("=============={}==================",jChannel.getViewAsString());
 
         } catch (Exception e) {
 
